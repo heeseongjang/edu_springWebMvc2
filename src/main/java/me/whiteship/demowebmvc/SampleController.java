514f39ab49dc17a54f2762faeebc597e3764276f
heeseong <wkdgmltjd1865@gmail.com>
2022-03-04 14:27:08 +0900

    요청 맵핑하기 3부 미디어 타입

--- src/main/java/me/whiteship/demowebmvc/SampleController.java
+++ src/main/java/me/whiteship/demowebmvc/SampleController.java
@@ -1,14 +1,17 @@
 package me.whiteship.demowebmvc;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleController {
 
-    @RequestMapping(value = "/hello", method = {RequestMethod.GET, RequestMethod.POST})
+    @GetMapping(
+            value = "/hello",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE)
     @ResponseBody
     public String hello() {
         return "hello";
--- src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
+++ src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
@@ -4,10 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,18 +22,30 @@ public class SampleControllerTest {
 
     @Test
     public void helloTest() throws Exception {
-        mockMvc.perform(get("/hello"))
+        mockMvc.perform(get("/hello")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON_VALUE))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string("hello"));
+                .andExpect(status().isOk());
 
-        mockMvc.perform(post("/hello"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string("hello"));
-
-        mockMvc.perform(put("/hello"))
-                .andDo(print())
-                .andExpect(status().isMethodNotAllowed());
+//        mockMvc.perform(get("/hello"))^
+//                .andDo(print())
+//                .andExpect(status().isOk())
+//                .andExpect(content().string("hello"));
+//
+//
+//        mockMvc.perform(get("/hello"))
+//                .andDo(print())
+//                .andExpect(status().isOk())
+//                .andExpect(content().string("hello"));
+//
+//        mockMvc.perform(post("/hello"))
+//                .andDo(print())
+//                .andExpect(status().isOk())
+//                .andExpect(content().string("hello"));
+//
+//        mockMvc.perform(put("/hello"))
+//                .andDo(print())
+//                .andExpect(status().isMethodNotAllowed());
     }
 }
