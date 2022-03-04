639c7f6458e2f4e2e2b10ec76cdc57fd6c8b6d34
heeseong <wkdgmltjd1865@gmail.com>
2022-03-04 16:47:04 +0900

    핸들러 메소드 3부 요청 매개변수 (단순 타입)

--- src/main/java/me/whiteship/demowebmvc/SampleController.java
+++ src/main/java/me/whiteship/demowebmvc/SampleController.java
@@ -1,19 +1,24 @@
 package me.whiteship.demowebmvc;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.MatrixVariable;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class SampleController {
 
     @GetMapping("/events/{id}")
     @ResponseBody
-    public Event deleteEvents(@PathVariable Integer id, @MatrixVariable String name) {
+    public Event getEvent(@PathVariable Integer id, @MatrixVariable String name) {
         Event event = new Event();
         event.setId(id);
+        event.setName(name);
+        return event;
+    }
+
+    @PostMapping("/events")
+    @ResponseBody
+    public Event insertEvent(@RequestParam(required = false, defaultValue = "heeseong") String name) {
+        Event event = new Event();
         event.setName(name);
         return event;
     }
--- src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
+++ src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,6 +25,15 @@ public class SampleControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("id").value(1));
+    }
+
+    @Test
+    public void insertEvent() throws Exception {
+        mockMvc.perform(post("/events")
+                        .param("name", "heeseong"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name").value("heeseong"));
 
     }
 }
