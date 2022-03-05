ab3d3d9825de9fbf93cab78c72f972ea1e608723
heeseong <wkdgmltjd1865@gmail.com>
2022-03-05 22:08:18 +0900

    핸들러 메소드 13부 MultipartFile

---
+++ src/test/java/me/whiteship/demowebmvc/FileControllerTest.java
@@ -0,0 +1,30 @@
+package me.whiteship.demowebmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FileControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void fileUploadFile() throws Exception {
+        MockMultipartFile file = new MockMultipartFile("file", "84.html", "plain/text","heee".getBytes());
+        this.mockMvc.perform(multipart("/file"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+    }
+}
